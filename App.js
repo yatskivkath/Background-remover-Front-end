02df245862e1cbe09a327356efb34bc2c8c2687d
kath <iamkate4@gmail.com>
2021-02-16 22:50:40 +0200

    Created a camera-app using expo-camera

--- App.js
+++ App.js
@@ -1,96 +1,11 @@
 import React from 'react';
-import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import logo from './assets/logo.png'; 
-import * as ImagePicker from 'expo-image-picker';
-import * as Sharing from 'expo-sharing'; 
 
-export default function App() {
-  const [selectedImage, setSelectedImage] = React.useState(null);
+import CameraPage from './src/camera.page';
 
-  let openImagePickerAsync = async () => {
-    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
-
-    if (permissionResult.granted === false) {
-      alert("Permission to access camera roll is required!");
-      return;
-    }
-
-    let pickerResult = await ImagePicker.launchImageLibraryAsync();
-    console.log(pickerResult);
-
-    if (pickerResult.cancelled === true) {
-      return;
-    }
-
-    setSelectedImage({ localUri: pickerResult.uri });
-  }
-
-  let openShareDialogAsync = async () => {
-    if (!(await Sharing.isAvailableAsync())) {
-      alert(`Uh oh, sharing isn't available on your platform`);
-      return;
-    }
-
-    await Sharing.shareAsync(selectedImage.localUri);
-  }; 
-  
-  if (selectedImage !== null) {
-    return (
-      <View style={styles.container}>
-        <Image
-          source={{ uri: selectedImage.localUri }}
-          style={styles.thumbnail}
-        />
-
-        <TouchableOpacity onPress={openShareDialogAsync} style={styles.button}>
-          <Text style={styles.btn}>Share this photo</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  }
-
-  return (
-    <View style={styles.container}>
-      <Image source={logo} style={styles.logo} /> 
-
-      <Text style={styles.text}>To share a photo from your phone with a friend, just press the button below!</Text>
-
-      <TouchableOpacity
-        onPress={openImagePickerAsync}
-        style={styles.btn}>
-        <Text style={{ fontSize: 20, color: '#fff' }}>Pick a photo</Text>
-      </TouchableOpacity>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  btn:{
-    backgroundColor: 'blue',
-    marginTop: 10,
-    padding: 20,
-    borderRadius: 5,
-    color: '#fff',
-  },
-  logo:{
-    width: 305, 
-    height: 159 
-  },
-  text: {
-    color: '#888', 
-    fontSize: 18,
-    marginHorizontal: 30,
-    marginTop: 30,
-  },
-  thumbnail: {
-    width: 300,
-    height: 300,
-    resizeMode: "contain"
-  }
-});
+export default class App extends React.Component {
+    render() {
+        return (
+            <CameraPage />
+        );
+    };
+};
